a9f75788d5ef8fdb3012523107c29f006400a03e
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-03 14:03:44 -0800

    added StockInfo component

---
+++ src/components/StockInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const StockPrice = ({ symbol, price, dailyChange }) => {
+  <figure>
+        Symbol:{symbol}
+        Price: {price}
+        Daily Change: {dailyChange}
+
+  </figure>;
+};
+
+export default StockPrice;
